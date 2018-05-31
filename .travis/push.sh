a2436d56dd00d3a27b1c32fa8953ca90bcf7d263
y1j2x34 <y1j2x34@qq.com>
2018-05-31 23:27:58 +0800

    fix: bad substitution

--- .travis/push.sh
+++ .travis/push.sh
@@ -13,16 +13,6 @@ log() {
 }
 
 ensure_preconditions_met() {
-#   if [ -z "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
-#     log "Job is CI for a push, skipping creation of production build"
-#     exit 0
-#   fi
-#   if [ "${TRAVIS_BRANCH}_${TRAVIS_PULL_REQUEST_BRANCH}" != "${TARGET_BRANCH}_${SOURCE_BRANCH}" ]; then
-#     log "Skipping creation of production build"
-#     log "We only create production builds for pull requests from '${SOURCE_BRANCH}' to '${TARGET_BRANCH}'"
-#     log "but this pull request is from '${TRAVIS_PULL_REQUEST_BRANCH}' to '${TRAVIS_BRANCH}'"
-#     exit 0
-#   fi
   if [ -z "${GITHUB_TOKEN}" ]; then
     log "GITHUB_TOKEN not set: won't be able to push production build"
     log "Please configure the token in .travis.yml or the Travis UI"
@@ -43,7 +33,7 @@ commit_build_files() {
 }
 
 push_to_github() {
-  git push --set-upstream origin-travis "${AUTOCOMMIT_BRANCH:SOURCE_BRANCH}"
+  git push --set-upstream origin-travis "${AUTOCOMMIT_BRANCH}:${SOURCE_BRANCH}"
 }
 
 ensure_preconditions_met
