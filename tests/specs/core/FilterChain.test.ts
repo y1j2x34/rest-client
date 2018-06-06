32f2c8eacd9c26ead9b47b70852929e79ee61054
y1j2x34 <y1j2x34@qq.com>
2018-06-07 00:25:55 +0800

    fix: compile error

--- tests/specs/core/FilterChain.test.ts
+++ tests/specs/core/FilterChain.test.ts
@@ -1,6 +1,6 @@
 import * as chai from 'chai';
 import * as spies from 'chai-spies';
-import FilterChain from '../../src/core/FilterChain';
+import FilterChain from '../../../src/core/FilterChain';
 
 chai.use(spies);
 
